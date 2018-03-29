a27794c5ae9bddd7039b2d30e2df7faf1790f7b2
User <User@User-PC>
2018-03-29 18:59:41 +0530

    push

--- src/test/java/freecrm/TasksTest.java
+++ src/test/java/freecrm/TasksTest.java
@@ -1,45 +1,40 @@
 package freecrm;
 
-import java.util.Iterator;
-import java.util.Set;
-
-import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 
 public class TasksTest extends lOGINTest {
-  @Test(priority=2)
-  public void f2() throws InterruptedException {
-	  driver.switchTo().frame("mainpanel");
-	  Actions act=new Actions(driver);
-	  WebElement ele=driver.findElement(By.xpath("//a[text()='Tasks']"));
-	  act.moveToElement(ele).build().perform();
-	  driver.findElement(By.xpath("//a[text()='New Task']")).click();
-	  Thread.sleep(2000);
-	  driver.findElement(By.name("title")).sendKeys("suguna");
-	  driver.findElement(By.xpath(".//*[@id='f_trigger_c_deadline']")).click();
-	  driver.findElement(By.xpath("(//td[text()='6'])[1]")).click();
-	  driver.findElement(By.name("auto_extend")).click();
-	  driver.findElement(By.xpath("(//input[@class='button_help'])[1]")).click();
-	 
+	
+  @Test(priority=1)
+  public void f1() throws InterruptedException {
 	  
-     // Alert s1=driver.switchTo().alert();
-    //  Thread.sleep(2000);
-     // s1.dismiss();
-//	  driver.findElement(By.xpath("//input[@type='button']")).click();
-	  
-	 Set< String> se=driver.getWindowHandles();
-	 Iterator<String> se1=se.iterator();
-	 String  parentId=se1.next();
-	 String childId=se1.next();
-	 driver.switchTo().window(childId);
-	 Thread.sleep(2000);
-	 driver.close();
-	 
-	  
-	 
-
+	driver.switchTo().frame("mainpanel");
+	Thread.sleep(2000);
+	driver.findElement(By.xpath("//a[@title='Tasks']")).click();
+	Thread.sleep(2000);
+	driver.findElement(By.name("cs_keyword")).sendKeys("asdfgasfhj");
+	driver.findElement(By.name("cs_extended")).sendKeys("dbhskk");
+	
+	driver.findElement(By.name("cs_completion")).sendKeys("70");
+	Thread.sleep(2000);
+	WebElement e= driver.findElement(By.xpath("//select[@name='cs_status']"));
+	e.click();
+	Select sel =new Select(e);
+	sel.selectByVisibleText("Complete");
+	Thread.sleep(2000);
+	WebElement e1= driver.findElement(By.xpath("//select[@name='cs_task_type']"));
+	e1.click();
+	Select sel1 =new Select(e1);
+	sel1.selectByVisibleText("Event");
+	Thread.sleep(2000);
+	
+	driver.findElement(By.name("cs_submit")).click();
+	
+	
+	
+	
+	
   }
 }
